1410c04ff9105bf9c976fa08d71890afdd6f7fb1
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-11-19 11:14:43 -0600

    Implemented function declarations to create a sleep debt calculator.

---
+++ project-sleep-debt.js
@@ -0,0 +1,64 @@
+function getSleepHours(day) {
+  switch(day) {
+    case 'monday':
+      return 8;
+      break;
+    case 'tuesday':
+      return 7;
+      break;
+    case 'wednesday':
+      return 7;
+      break;
+    case 'thursday':
+      return 4;
+      break;
+    case 'friday':
+      return 5;
+      break;
+    case 'saturday':
+      return 6;
+      break;
+    case 'sunday':
+      return 10;
+      break;
+    default:
+      return 'Day input is incorrect.';
+  }
+
+}
+
+//console.log(getSleepHours('sunday'));
+
+function getActualSleepHours() {
+  a = getSleepHours('monday');
+  b = getSleepHours('tuesday');
+  c = getSleepHours('wednesday');
+  d = getSleepHours('thursday');
+  e = getSleepHours('friday');
+  f = getSleepHours('saturday');
+  g = getSleepHours('sunday');
+  totalHours = a + b + c + d + e + f + g;
+  return totalHours;
+}
+
+function getIdealSleepHours() {
+  idealHours = 8;
+  return idealHours * 7;
+}
+
+function calculateSleepDebt() {
+  let actualSleepHours = getActualSleepHours();
+  let idealSleepHours = getIdealSleepHours();
+  if (actualSleepHours === idealSleepHours) {
+    console.log('You have the perfect amount of sleep.');
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log('You are sleeping more than needed.');
+  } else {
+    console.log('You should get some rest');
+  }
+}
+
+console.log(getIdealSleepHours());
+console.log(getActualSleepHours());
+calculateSleepDebt();
+//let getSleepHours = day => {};
