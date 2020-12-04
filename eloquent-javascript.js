0a47b513ff9862c7cdd43e9771bfc330341b95bb
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-04 13:53:39 -0600

    Added function that prints numbers from 1-100 except n%3, n%5 but not n%3, instead prints fizz and buzz

--- eloquent-javascript.js
+++ eloquent-javascript.js
@@ -3,3 +3,13 @@ for (let counter = 0; counter < 7; counter++) {
     console.log(hashTag);
     hashTag = hashTag + '#';
 }
+
+for(let counter = 1; counter <= 100; counter++) {
+    if (counter % 3 === 0) {
+        console.log("Fizz");
+    } else if (counter % 5 === 0 && counter % 3 !== 0) {
+        console.log('Buzz');
+    } else {
+        console.log(counter);
+    }
+}
