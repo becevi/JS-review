bd565869cecdad45cbf99f00a1e9c5df7d9c72b2
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-04 14:36:59 -0600

    Created a loop that logs a grid given a even number as a size

--- eloquent-javascript.js
+++ eloquent-javascript.js
@@ -15,4 +15,27 @@ for(let counter = 1; counter <= 100; counter++) {
         console.log(counter);
     }
 }
+let size = 8;
+for (let counter = 0; counter < size; counter++) {
+    const line1 = ' #';
+    const line2 = '# ';
+
+
+}
+
+let size = 14;
+let char1 = ' #';
+let char2 = '# ';
+let line1 = '';
+let line2 = '';
+while (line1.length <= size) {  
+    line1 = line1 + char1;
+    line2 = line2 + char2;
+    if (line1.length === size) {
+        for (let counter = 0; counter <= size; counter += 2 ){
+            console.log(line1);
+            console.log(line2);
+        }  
+    }
+}
 
