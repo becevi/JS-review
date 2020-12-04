8107f53366923af80dff31fbcf40af71e3065bac
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-04 13:57:52 -0600

    Added n%5 = 0 && n%3 = 0 condition

--- eloquent-javascript.js
+++ eloquent-javascript.js
@@ -5,11 +5,14 @@ for (let counter = 0; counter < 7; counter++) {
 }
 
 for(let counter = 1; counter <= 100; counter++) {
-    if (counter % 3 === 0) {
-        console.log("Fizz");
+    if (counter % 3 === 0 && counter % 5 === 0) {
+        console.log("FizzBuzz");
     } else if (counter % 5 === 0 && counter % 3 !== 0) {
         console.log('Buzz');
-    } else {
+    } else if (counter % 3 === 0) {
+        console.log("Fizz");
+    } else if {
         console.log(counter);
     }
 }
+
