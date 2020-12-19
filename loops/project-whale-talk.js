23c8146511136954c9e9c870e44e75e563cb4653
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-18 19:44:46 -0600

    Added comments

--- loops/project-whale-talk.js
+++ loops/project-whale-talk.js
@@ -1,9 +1,15 @@
+// declared input variable
 let input = 'Hello stranger';
 
+// declared array of vowels
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
+// declared empty array
 let resultArray = [];
 
+// created for loop. first part is the counter, then is the condition
+// to be evaluated and then the increment.
+// the order is counter, condition, code, increment
 for (i = 0; i < input.length; i++) {
   //console.log(input[i]);
   for (j = 0; j < vowels.length; j++) {
