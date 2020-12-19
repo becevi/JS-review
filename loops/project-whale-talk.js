96f4dfea536cf929ab0812393537e1f8a351ed0e
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-19 11:04:22 -0600

    Finished comments

--- loops/project-whale-talk.js
+++ loops/project-whale-talk.js
@@ -10,6 +10,10 @@ let resultArray = [];
 // created for loop. first part is the counter, then is the condition
 // to be evaluated and then the increment.
 // the order is counter, condition, code, increment
+// in this case we have nested for loops. the first one goes through each
+// character of the string, for each character then it loops against
+// each vowel and if. if the character is e or u it adds it twice, else 
+// it only adds one time the vowel of the string
 for (i = 0; i < input.length; i++) {
   //console.log(input[i]);
   for (j = 0; j < vowels.length; j++) {
